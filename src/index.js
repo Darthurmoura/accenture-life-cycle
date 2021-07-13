0fb0c7cecbfd2f4cae3818e0322a206a0e652480
Daniel Moura <darthur.moura@gmail.com>
2021-07-13 19:31:49 -0300

    First commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+class App extends React.Component {
+  state = {
+    clicou: 'ainda não clicou',
+  }
+
+  componentDidMount() {
+    console.log('[APP] - Componente foi montado');
+  }
+
+  componentDidUpdate() {
+    console.log('[APP] - Componente foi atualizado');
+  }
+
+  handleClick() {
+    this.setState({
+      clicou: 'clicou',
+    });
+  }
+
+  handleTextChange(e) {
+    this.setState({
+      texto: e.target.value,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Dale, {this.props.stuff.name}.</h1>
+        <h2>Tu tem {this.props.stuff.age} anos.</h2>
+        <p>Você {this.state.clicou}</p>
+        <button onClick={this.handleClick.bind(this)}>Exibir alerta</button>
+        <h3>{this.state.texto}</h3>
+      </div>
+    )
+  }
+}
+
+const stuff = {
+  name: 'Daniel',
+  age: 35,
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App stuff={stuff} />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
